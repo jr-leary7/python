c5d1f7870809bb54715ec01697fdc087d714065a
Jack Leary <jrleary@live.unc.edu>
2020-06-12 10:58:23 -0400

    wrapped OLS into function

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -6,11 +6,17 @@ import numpy as np
 
 # generate random X matrix and Y vector from Gaussian 
 nums = np.random.normal(loc=0, scale=1, size=100)
-X = np.array(nums.reshape(10, 10))
-Y = np.random.normal(loc=1, scale=2, size=10)
+x = nums.reshape(10, 10)
+y = np.random.normal(loc=1, scale=2, size=10)
 
-# solve
-B = np.linalg.inv(X.transpose().dot(X)).dot(X.transpose().dot(Y))
-print(B)
+# implement as function
+def solveOLS(X, Y):
+    X = np.array(X)
+    Y = np.array(Y)
+    B = np.linalg.inv(X.transpose().dot(X)).dot(X.transpose().dot(Y))
+    return B;
+
+beta = solveOLS(X=x, Y=y)
+print(beta)
 
 # TODO - add bootstrap procedue to estimate p-values for \hat{Beta}
